11d2f7ff3964d550e94951199518b1e039d897f8
Natã Bento <73320913+natabsa@users.noreply.github.com>
2026-06-08 10:17:32 -0300

    altered loop condition in extended Euclidean algorithm

--- Trabalho I/main.py
+++ Trabalho I/main.py
@@ -27,11 +27,10 @@ def ext_euclides(a, b):
     a, b = gmpy2.mpz(a), gmpy2.mpz(b)
     x0, x1 = gmpy2.mpz(1), gmpy2.mpz(0)
     y0, y1 = gmpy2.mpz(0), gmpy2.mpz(1)
-    r = b
-    while b > 0:
+    while (r = a % b) > 0:
         # a = b*q + r
         q = a // b
-        a, b = b, a % b
+        a, b = b, r
         x0, x1 = x1, x0 - q * x1
         y0, y1 = y1, y0 - q * y1
         
